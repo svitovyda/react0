340f7787cadc55f87c964a63d4fefdd49cddf5b5
Romanna Koval <rkoval@evolutiongaming.com>
2021-10-09 22:40:50 +0200

    Add tests, style, svg's, images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import * as React from "react";
 import { TestComponent } from "./TestComponent";
+import Image from "./assets/gears2.svg";
 
 export const App: React.FC<{}> = () => {
-  return <TestComponent text="Hello World!" />;
+  return (
+    <>
+      <Image width="80" height="80" color="#ffffff" />
+      <TestComponent text="Hello World!" />
+    </>
+  );
 };
 
 App.displayName = "App";
